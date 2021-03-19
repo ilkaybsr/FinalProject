cb2ed8edad57a77b7f7baa2e842e0afa50e40c23
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-03-19 13:15:36 +0300

    Core katmanı oluşturuldu

--- FinalProject/ConsoleUI/Program.cs
+++ FinalProject/ConsoleUI/Program.cs
@@ -9,7 +9,17 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EFProductDal());
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAll())
             {
